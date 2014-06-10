b97cd77ac63e2848bab07bf874e37c07e126cb7e
Steve Smith <steve@kentsmiths.com>
2014-06-10 16:56:25 -0400

    First Commit
    First test passes.

---
+++ MicrosoftVersioning/UnitTests/VersionIntShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MicrosoftVersioning.UnitTests
+{
+    [TestFixture]
+    class VersionIntShould
+    {
+        [Test]
+        public void BeCreatedAndComparableAsInt()
+        {
+            VersionInt version = 10;
+
+            Assert.AreEqual(10, version);
+        }
+    }
+}
---
+++ MicrosoftVersioning/VersionInt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftVersioning
+{
+    public struct VersionInt
+    {
+        private int _value;
+
+        public static implicit operator VersionInt(int value)
+        {
+            return new VersionInt{_value = value};
+        }
+
+        public static implicit operator int(VersionInt value)
+        {
+            return value._value;
+        }
+    }
+}
